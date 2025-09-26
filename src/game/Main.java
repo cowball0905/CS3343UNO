973b7972c3d06a6842789298244e38a55cf0fe1e
Mitchchch <wingki083@gmail.com>
2025-09-26 16:04:08 +0800

    test

--- src/game/Main.java
+++ src/game/Main.java
@@ -3,7 +3,7 @@ package game;
 public class Main {
     // main function that starts the UNO game
     public static void main(String[] args) {
-        // Start the UNO game
+        // Start the UNO game test
         UNOController uno = UNOController.getInstance();
     }
 }
