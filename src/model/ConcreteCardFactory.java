acb7b1f03d264bb33e15f810925b192d6f61009b
Mitchchch <wingki083@gmail.com>
2025-11-21 10:36:16 +0800

    Factory refactor

--- src/model/ConcreteCardFactory.java
+++ src/model/ConcreteCardFactory.java
@@ -13,32 +13,16 @@ public class ConcreteCardFactory extends CardFactory {
     @Override
     public Card createCard(HashMap<String, Integer> Deck, Boolean isTop, boolean isRevealed, String cardString) {
         String card = null;
+        
         if (isTop) {
-            ArrayList<String> numberCards = new ArrayList<>();
-            for (String key : Deck.keySet()) {
-                if (!key.equals("WildCard") && !key.equals("WildDrawFour") && 
-                    !key.contains("Skip") && !key.contains("Reverse") && !key.contains("DrawTwo")) {
-                    numberCards.add(key);
-                }
-            }
-            
+            ArrayList<String> numberCards = getNumberCards(Deck);
             if (numberCards.isEmpty()) {
                 return null;
             }
-            
-            int index = random.nextInt(numberCards.size());
-            card = numberCards.get(index);
-            
-            Deck.put(card, Deck.get(card) - 1);
-            if (Deck.get(card) == 0) {
-                Deck.remove(card);
-            }
+            card = getRandomCard(Deck, numberCards);
         } else if (!cardString.isEmpty() && Deck.containsKey(cardString) && Deck.get(cardString) > 0) { // create specific card
             card = cardString;
-            Deck.put(card, Deck.get(card) - 1);
-            if (Deck.get(card) == 0) {
-                Deck.remove(card);
-            }
+            removeCardFromDeck(Deck, card);
         } else {
             if (!cardString.isEmpty()) {
                 System.out.println("Deck doesn't contain this card!!!");
@@ -47,49 +31,72 @@ public class ConcreteCardFactory extends CardFactory {
             if (availableCards.isEmpty()) {
                 return null;
             }
-            int index = random.nextInt(availableCards.size());
-            card = availableCards.get(index);
-            
-            Deck.put(card, Deck.get(card) - 1);
-            if (Deck.get(card) == 0) {
-                Deck.remove(card);
-            }
+            card = getRandomCard(Deck, availableCards);
         }
 
         if (card == null) {
             return null;
         }
         
+        return createSpecificCard(card, isRevealed);
+    }
+
+    private ArrayList<String> getNumberCards(HashMap<String, Integer> Deck) {
+        ArrayList<String> numberCards = new ArrayList<>();
+            for (String key : Deck.keySet()) {
+                if (!key.equals("WildCard") && !key.equals("WildDrawFour") && 
+                    !key.contains("Skip") && !key.contains("Reverse") && !key.contains("DrawTwo")) {
+                    numberCards.add(key);
+                }
+            }
+        return numberCards;
+    }
+
+    private void removeCardFromDeck(HashMap<String, Integer> Deck, String card) {
+        Deck.put(card, Deck.get(card) - 1);
+        if (Deck.get(card) == 0) {
+            Deck.remove(card);
+        }
+    }
+
+    private String getRandomCard(HashMap<String, Integer> Deck, ArrayList<String> Cards) {
+        int index = random.nextInt(Cards.size());
+        String card = Cards.get(index);
+        removeCardFromDeck(Deck, card);
+        return card;
+    }
+
+    private Card createSpecificCard(String card, boolean isRevealed) {
         if (card.equals("WildCard")){
             return new WildCard(isRevealed);
-        } else if (card.equals("WildDrawFour")){
+        } 
+        if (card.equals("WildDrawFour")){
             return new WildDrawFourCard(isRevealed);
-        } else {
-            Color cardcolor;
-            switch (card.charAt(0)){
-                case 'r':
-                    cardcolor = Color.Red;   
-                    break;
-                case 'b':
-                    cardcolor = Color.Blue;
-                    break;
-                case 'y':
-                    cardcolor = Color.Yellow;
-                    break;
-                default:
-                    cardcolor = Color.Green;
-                    break;
-            }
-            switch (card.substring(1)) {
-                case "Skip":
-                    return new SkipCard(cardcolor, isRevealed);
-                case "Reverse":
-                    return new ReverseCard(cardcolor, isRevealed);
-                case "DrawTwo":
-                    return new DrawTwoCard(cardcolor, isRevealed);
-                default:
-                    return new NumberCard(cardcolor, Integer.parseInt(card.substring(1)), isRevealed);
-            }
+        }
+
+        Color cardcolor = getColor(card);
+        switch (card.substring(1)) {
+            case "Skip":
+                return new SkipCard(cardcolor, isRevealed);
+            case "Reverse":
+                return new ReverseCard(cardcolor, isRevealed);
+            case "DrawTwo":
+                return new DrawTwoCard(cardcolor, isRevealed);
+            default:
+                return new NumberCard(cardcolor, Integer.parseInt(card.substring(1)), isRevealed);
+        }
+    }
+
+    private Color getColor(String card) {
+        switch (card.charAt(0)){
+            case 'r':
+                return Color.Red;   
+            case 'b':
+                return Color.Blue;
+            case 'y':
+                return Color.Yellow;
+            default:
+                return Color.Green;
         }
     }
 }
