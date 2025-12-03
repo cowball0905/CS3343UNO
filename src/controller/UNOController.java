e7058f148f92b7d6476d0a94284e19060add5e38
Mitchchch <wingki083@gmail.com>
2025-12-03 21:47:04 +0800

    comment issue

--- src/controller/UNOController.java
+++ src/controller/UNOController.java
@@ -347,7 +347,6 @@ public class UNOController {
     }
 
     public boolean canPlayCard(Card matchCard, Card topCard) {
-        // 如果没有顶牌（游戏刚开始或PlayedCard为空），允许任何牌
         if (topCard == null || matchCard == null) {
             return true;
         }
